f59f81e612bec2e501584793a3133a037b1a5f46
Mahmut Bulut <scienceblock@gmail.com>
2011-06-23 05:07:17 +0300

    main file

--- lib/main.rb
+++ lib/main.rb
@@ -11,3 +11,39 @@ puts "===RubMat===
     This program comes with ABSOLUTELY NO WARRANTY; for details type 'help'.
     This is free software, and you are welcome to redistribute it
     under certain conditions; type `certain' for details."
+
+
+def commsuccss
+  # Return success statement and new command_line
+end
+
+def commfail
+  # Raise exceptions from fail situation and gives new command_line
+end
+
+class GetOpt
+  def command_line
+  print ">=> "
+  comm = gets
+  commnd = comm.chomp
+  puts ""
+  end
+  def clearscr
+    # Clears screen
+  end
+  
+end
+
+
+def inspect
+  # Read arguments and defines commands
+  # Will handle command line whitespaces
+  
+  if (commnd == 'help')
+    # Help of commands and usages
+  end
+  if (commnd == 'certain')
+    # Certain conditions
+  end
+end
+
